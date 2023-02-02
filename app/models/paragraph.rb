4f359671b9e1c12000b323dac927b27caab0af22
Alexander Bragin <abragin@gmail.com>
2023-02-02 17:20:47 +0400

    Update: strip paragraph's content on creation

--- app/models/paragraph.rb
+++ app/models/paragraph.rb
@@ -1,4 +1,9 @@
 class Paragraph < ApplicationRecord
   belongs_to :chapter
+  before_create :strip_content
   scope :active, -> { where(ignore: false) }
+
+  def strip_content
+    content.strip!
+  end
 end
--- test/models/paragraph_test.rb
+++ test/models/paragraph_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ParagraphTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "content is stripped on create" do
+    p = Paragraph.new(
+      chapter: chapters(:one),
+      position: 10,
+      content: " \n  content \n"
+    )
+    p.save
+    assert_equal "content", p.content
+  end
 end
