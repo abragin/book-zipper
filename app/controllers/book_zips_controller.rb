861df36993f42f75a1c77b0dd5eae12a4bc3aee0
Alexander Bragin <abragin@gmail.com>
2023-02-05 21:06:03 +0400

    Update: new BookZip from Book#show page + BookZip destroy action

--- app/controllers/book_zips_controller.rb
+++ app/controllers/book_zips_controller.rb
@@ -1,14 +1,17 @@
 class BookZipsController < ApplicationController
+  before_action :set_book_zip, only: %i[ show destroy ]
 
   def index
     @book_zips = BookZip.all
   end
 
   def new
+    @book = Book.find(params[:book_id])
     @book_zip = BookZip.new
   end
 
   def create
+    @book = Book.find(params[:book_id])
     @book_zip = BookZip.new(book_zip_params)
     if @book_zip.save
       redirect_to @book_zip
@@ -18,10 +21,22 @@ class BookZipsController < ApplicationController
   end
 
   def show
-    @book_zip = BookZip.find(params[:id])
+  end
+
+  def destroy
+    @book_zip.destroy
+
+    respond_to do |format|
+      format.html { redirect_to book_zips_url, notice: "Book zip was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
+
+  def set_book_zip
+    @book_zip = BookZip.find(params[:id])
+  end
 
   def book_zip_params
     params.require(:book_zip).permit(
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   resources :languages
-  resources :books
+  resources :books do
+    resources :book_zips, only: [:new, :create]
+  end
+
   resources :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #get "/epub_books", to: "epub_books#index"
   root "epub_books#index"
 
-  resources :book_zips, only: [:index, :new, :create, :show] do
+  resources :book_zips, only: [:index, :show, :destroy] do
     resources :chapter_zips, only: [:new, :create, :edit, :update, :destroy]
   end
 
