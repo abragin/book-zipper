8bd8896d21d4d7427163bcf0a831c309715e56f6
Alexander Bragin <abragin@gmail.com>
2023-02-05 21:08:26 +0400

    Fix: cleaned unused references in BookZip

--- app/models/book_zip.rb
+++ app/models/book_zip.rb
@@ -1,8 +1,6 @@
 class BookZip < ApplicationRecord
   belongs_to :ebook_source, class_name: "EpubBook"
   belongs_to :ebook_target, class_name: "EpubBook"
-  has_one :current_source_paragraph, class_name: "Paragraph"
-  has_one :current_target_paragraph, class_name: "Paragraph"
   has_many :chapter_zips,
     -> { order('position') },
      dependent: :destroy
